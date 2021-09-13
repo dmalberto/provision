3c16646c8a8e93f0197c394bd958443e0afd9a5e
Diego Alberto <dmalberto@outlook.com>
2021-09-13 04:42:09 +0000

    fix: remove configurações não utilizadas

--- script.sh
+++ script.sh
@@ -1,20 +1,22 @@
 sudo apt update -y && apt upgrade -y
-sudo apt install zsh -y
 sudo apt install git -y
-sudo apt install vim -y
 sudo apt install curl -y
 sudo apt install wget -y
 sudo apt install tmux -y
-sudo apt install -y python3-pip
-sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
-sudo apt install -y python3-venv
-sudo apt install awscli -y
 
-
+# configs
 cp zshrc ~/.zshrc
 cp vimrc ~/.vimrc
 cp gitconfig ~/.gitconfig
 cp p10k ~/.p10k.zsh
+
+# aws
+sudo apt install awscli -y
+
+# python3
+sudo apt install -y python3-pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+sudo apt install -y python3-venv
 
 # docker
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -26,18 +28,15 @@ sudo apt -y install docker-ce
 # firebase
 curl -sL https://firebase.tools | bash
 
-#Font
-sudo apt install fonts-firacode -y
-
-# Node
+# node
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 chmod 777 ~/.nvm
 source ~/.bashrc
 nvm install node -y
 npm install --global yarn
 
-
 # zsh
+sudo apt install zsh -y
 sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
@@ -48,6 +47,8 @@ exit
 zsh
 source ~/.zshrc
 
+# vim
+sudo apt install vim -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -56,9 +57,8 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
+
 # # vscode
 # apt install snapd
 # snap install code --classic
 
-# # git
-# curl https://raw.githubusercontent.com/dmalberto/dotfiles/master/.gitconfig -o ~/.gitconfig
--- zshrc
+++ zshrc
@@ -26,23 +26,6 @@ dnf
 
 source $ZSH/oh-my-zsh.sh
 
-SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  exec_time     # Execution time
-  line_sep      # Line break
-  vi_mode       # Vi-mode indicator
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
-)
-SPACESHIP_USER_SHOW=always
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_CHAR_SYMBOL="❯"
-SPACESHIP_CHAR_SUFFIX=" "
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
